7b1470960e858939b91a0e0f604e98d5b337113f
artem.shynkarenko <artem.shynkarenko@0d0ad13b-7e51-0410-977d-d3f3eabcdc60>
2008-08-15 16:22:09 +0000

    Added new files

---
+++ remoteadmin/Interlogic.Trainings.Plugs.Kernel/Exceptions/ValidationException.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interlogic.Trainings.Plugs.Kernel.Exceptions
+{
+	public class ValidationException:Exception
+	{
+	}
+}
---
+++ remoteadmin/Interlogic.Trainings.Plugs.Kernel/PlugLocationFactory.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Interlogic.Trainings.Plugs.Kernel.DomainModel;
+
+namespace Interlogic.Trainings.Plugs.Kernel
+{
+	public class PlugLocationFactory:DomainFactory
+	{
+		public override void InstallRequiredEnvironment(Interlogic.Trainings.Plugs.Kernel.SqlActions.ISqlTransactionContext context)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		public override void UpdateRequiredEnvironment(Interlogic.Trainings.Plugs.Kernel.SqlActions.ISqlTransactionContext context)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		public override void UninstallRequiredEnvironment(Interlogic.Trainings.Plugs.Kernel.SqlActions.ISqlTransactionContext context)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		internal void InternalInsert(PlugLocation location)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		internal static PlugLocationFactory GetInstance()
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+	}
+}
