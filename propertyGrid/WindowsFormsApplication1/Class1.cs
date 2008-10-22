33cad0df8e01b82e01267b9ba16bf9a2f03cac4a
artem.shynkarenko <artem.shynkarenko@0d0ad13b-7e51-0410-977d-d3f3eabcdc60>
2008-10-22 15:38:18 +0000

    Added test application and working folder for ASP.NET property grid start-up

---
+++ propertyGrid/WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Windows.Forms.Design;
+
+namespace WindowsFormsApplication1
+{
+	public struct Struct1
+	{
+		int z;
+		int x;
+		int y;
+
+		public override string ToString()
+		{
+			return string.Format("({0},{1},{2})", x, y, z); ;
+		}
+
+	}
+	
+	public class Class1
+	{
+		[Category("First category")]
+		public string MyString { get; set; }
+
+		[Category("First category")]
+		[DisplayName("This is not propertyName")]
+		[Description("My long description")]
+		public int MyInt { get; set; }
+
+		[Category("First category")]
+		public DateTime MyDateTime { get; set; }
+
+		[Category("Second category")]
+		public List<Class1> MyList { get; set; }
+
+		public enum TestEnum
+		{
+			One, Two, Three
+		}
+
+		[Category("Second category")]
+		public TestEnum MyEnum { get; set; }
+
+		[Browsable(false)]
+		public int MyHidden { get; set; }
+
+		[Category("Second category")]
+		public int MyReadonly { get; private set; }
+
+		[Editor(typeof(FileNameEditor), typeof(UITypeEditor))]
+		public string MyPath { get; set; }
+
+		public Struct1 MyStruct { get; set; }
+
+	}
+}
