41cf908599bbde0523d89b62e3712b75bb8f598f
artem.shynkarenko <artem.shynkarenko@0d0ad13b-7e51-0410-977d-d3f3eabcdc60>
2009-02-05 16:30:24 +0000

    Updated classes

--- remoteadmin/Interlogic.Trainings.Plugs.Kernel/Install/PlugInstaller.cs
+++ remoteadmin/Interlogic.Trainings.Plugs.Kernel/Install/PlugInstaller.cs
@@ -6,9 +6,10 @@ namespace Interlogic.Trainings.Plugs.Kernel
 {
 	public abstract class PlugInstaller
 	{
-		public abstract List<IRegisterPlugAction> RegisterActions { get;}
-		public abstract List<IUpdatePlugAction> UpdateActions { get;}
-		public abstract List<IUnregisterPlugAction> UnregisterActions { get;}
+		//TODO: |Do they realy needed?
+		//public abstract List<IRegisterPlugAction> RegisterActions { get;}
+		//public abstract List<IUpdatePlugAction> UpdateActions { get;}
+		//public abstract List<IUnregisterPlugAction> UnregisterActions { get;}
 
 		public abstract void RegisterPlug();
 		public abstract void UpdatePlug();
