583f54345957473a15f3daf9a4ebd0c69298d5d4
misha.gm@gmail.com <misha.gm@gmail.com@0d0ad13b-7e51-0410-977d-d3f3eabcdc60>
2009-02-19 20:14:23 +0000

    Installer: Add installer form. User can choose SQL Server and database.

--- trunk/remoteadmin/Interlogic.Trainings.Plugs.InstallApp/Form1.Designer.cs
+++ trunk/remoteadmin/Interlogic.Trainings.Plugs.InstallApp/Form1.Designer.cs
@@ -28,12 +28,198 @@ namespace Interlogic.Trainings.Plugs.InstallApp
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.DBName = new System.Windows.Forms.ComboBox();
+            this.SQLServer = new System.Windows.Forms.ComboBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OK = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(6, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(285, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(136, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Open existing database";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(126, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Create new database";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.DBName);
+            this.panel2.Controls.Add(this.SQLServer);
+            this.panel2.Controls.Add(this.Password);
+            this.panel2.Controls.Add(this.Username);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(6, 60);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(285, 120);
+            this.panel2.TabIndex = 1;
+            // 
+            // DBName
+            // 
+            this.DBName.FormattingEnabled = true;
+            this.DBName.Location = new System.Drawing.Point(77, 40);
+            this.DBName.Name = "DBName";
+            this.DBName.Size = new System.Drawing.Size(197, 21);
+            this.DBName.TabIndex = 9;
+            this.DBName.DropDown += new System.EventHandler(this.DBName_DropDown);
+            // 
+            // SQLServer
+            // 
+            this.SQLServer.FormattingEnabled = true;
+            this.SQLServer.Location = new System.Drawing.Point(77, 18);
+            this.SQLServer.Name = "SQLServer";
+            this.SQLServer.Size = new System.Drawing.Size(197, 21);
+            this.SQLServer.TabIndex = 8;
+            this.SQLServer.SelectedIndexChanged += new System.EventHandler(this.SQLServer_DropDown );
+            this.SQLServer.DropDown += new System.EventHandler(this.SQLServer_DropDown);
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(77, 86);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '?';
+            this.Password.Size = new System.Drawing.Size(197, 20);
+            this.Password.TabIndex = 7;
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(77, 63);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(197, 20);
+            this.Username.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(7, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DB name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SQL server:";
+            // 
+            // OK
+            // 
+            this.OK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OK.Location = new System.Drawing.Point(146, 186);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(68, 25);
+            this.OK.TabIndex = 2;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel.Location = new System.Drawing.Point(220, 186);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(68, 25);
+            this.Cancel.TabIndex = 3;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 219);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.ComboBox SQLServer;
+        private System.Windows.Forms.ComboBox DBName;
 	}
 }
 
