53bbfa12d0fe965d08bb938ee02d56a7d1981374
artem.shynkarenko <artem.shynkarenko@0d0ad13b-7e51-0410-977d-d3f3eabcdc60>
2009-02-20 15:38:23 +0000

    Added show cases for basic functionality of installer application

---
+++ trunk/remoteadmin/Interlogic.Trainings.Plugs.InstallApp/ProgressForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Interlogic.Trainings.Plugs.InstallApp
+{
+	public partial class ProgressForm : Form
+	{
+		public ProgressForm()
+		{
+			InitializeComponent();
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
